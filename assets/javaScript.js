049ed760bcf5d0bbde837471fc4e32bc3d6d9789
larissadiasdesena <larisenna17@gmail.com>
2022-06-10 21:01:52 -0300

    em processo
    Adição do boneco na forca

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,160 +1,136 @@
 //Variáveis pra poder puxar nas funções
-var tentativas = 6;
+
 var acertos = 0;
-var imagem = 0;
-let posicao; 
+var erros = 0;
+let acertou = false;
+var perdeu = false;
+var ganhou = false;
+let posicao;
 var categoriaDaPalavra;
 var sorteioDaPalavra;
+var caminhoImagem = ["cabeca.png", "corpo.png", "braco1.png", "braco2.png", "perna1.png", "completo.png"]
 
-var total = document.querySelector("#nvJogo");
-
-
-
+var novoJogo = document.querySelector("#nvJogo");
+var desistir = document.querySelector("#desistir");
 
 
 // nome e categoria das palavras que vão ser sorteadas
 const palavrasDaForca = [
-    { nome : "BRASIL",
-      categoria : "PAIS"},
+  {
+    nome: "BRASIL",
+    categoria: "PAIS"
+  },
 
-    { nome: "PIZZA",
-      categoria: "COMIDA"},
+  {
+    nome: "PIZZA",
+    categoria: "COMIDA"
+  },
 
-    { nome: "LAPIS",
-      categoria: "OBJETO"},
+  {
+    nome: "LAPIS",
+    categoria: "OBJETO"
+  },
 
-    { nome: "RETRATO",
-      categoria: "OBJETO"},
+  {
+    nome: "RETRATO",
+    categoria: "OBJETO"
+  },
 
-    { nome: "PARQUE",
-     categoria: "LUGAR"},
+  {
+    nome: "PARQUE",
+    categoria: "LUGAR"
+  },
 
-    { nome: "RELOGIO",
-      categoria: "OBJETO"},
+  {
+    nome: "RELOGIO",
+    categoria: "OBJETO"
+  },
 ];
 
 function criarPalavra() {
-    let indexPalavra = parseInt(Math.random() * palavrasDaForca.length)
+  let indexPalavra = parseInt(Math.random() * palavrasDaForca.length)
 
-     
-    sorteioDaPalavra = palavrasDaForca[indexPalavra].nome
-    categoriaDaPalavra = palavrasDaForca[indexPalavra].categoria
-    //return palavrasDaForca[indexPalavra];
-    
+  sorteioDaPalavra = palavrasDaForca[indexPalavra].nome
+  categoriaDaPalavra = palavrasDaForca[indexPalavra].categoria
+  //return palavrasDaForca[indexPalavra];
+
 }
 criarPalavra() // chamar a função pra que ela apareça
 
 // mandar pro html os valores pra que apareçam na tela.
 function palavraNaTela() {
-    let categoria = document.getElementById("dica");
-    categoria.innerHTML = categoriaDaPalavra
-    
-    let nome = document.getElementById("palavraSecreta");
-    nome.innerHTML = "";
+  let categoria = document.getElementById("dica");
+  categoria.innerHTML = categoriaDaPalavra
 
-   
-        for(let numero = 0; numero < sorteioDaPalavra.length; numero++){
-            const element = document.createElement("div") // criando um elemento filho na div de acordo com o tamanho da palavra
-            element.classList.add("letra")
-            document.getElementById("palavraSecreta").appendChild(element) 
-            
-        }
-     
+  let nome = document.getElementById("palavraSecreta");
+  nome.innerHTML = "";
 
+
+  for (let numero = 0; numero < sorteioDaPalavra.length; numero++) {
+    const element = document.createElement("div") // criando um elemento filho na div de acordo com o tamanho da palavra
+    element.classList.add("letra")
+    document.getElementById("palavraSecreta").appendChild(element)
+
+  }
 }
 
-palavraNaTela() 
+palavraNaTela()
 
-    
-let acertou = false;                                                                                                                                                        
 
 // mostrando que a palavra apertada esta certa.
 function letraMarcada(letra) {
-
-  
+  acertou;
 
   let botao = document.getElementById(letra);
   botao.setAttribute('class', 'certa'); // criando um class pra esilisar no css mostrando a tecla apertada
-  
+
   let letrinha = sorteioDaPalavra.toLocaleUpperCase() // garantir que a letra fique maiúscula
-  
 
-    for(let i =  0; i < letrinha.length; i++){
-        if(letrinha.charAt(i) == letra){ 
-          document.querySelectorAll("#palavraSecreta div")[i].innerHTML = letrinha.charAt(i) // ele ta pegando a div de palavraSecreta e transformando em uma array - pra adicionar a letra se a tecla apertada tiver certa pra continuar mostrando
-           
-          botao.removeAttribute('onclick'); // remover a função de cliq do botão pra que não seja clicado novamente
-            
-          acertos++;
-          acertou = true;
 
-  
-        }
-       
+  for (let i = 0; i < letrinha.length; i++){
+    if (letrinha.charAt(i) == letra) {
+      document.querySelectorAll("#palavraSecreta div")[i].innerHTML = letrinha.charAt(i) // ele ta pegando a div de palavraSecreta e transformando em uma array - pra adicionar a letra se a tecla apertada tiver certa pra continuar mostrando
+
+      botao.removeAttribute('onclick'); // remover a função de cliq do botão pra que não seja clicado novamente
+
+      acertos++;
+      acertou = true;
     }
-
-  }
-       
-         
- 
-
-       
-
-
-            
-          
-          
-          
- 
-
-
-
-/*
- 
-
-
-
-
-
-
-
-
-  if (chances === 0) {
-      let mensagem = document.createElement("p");
-      let t1 = document.createTextNode("Você perdeu!");
-      mensagem.appendChild(t1);
-
-      let botao = document.createElement("button");
-      let t2 = document.createTextNode("jogar novamente");
+  }if (!acertou) { // 
+    if (erros == 0) {
+      document.getElementById("containerForca").classList.add("mostrar")
+    }
+  
+    if (erros < 6) {
       
-      botao.appendChild(t2);
-      botao.setAttribute('class', 'novo-bt');
-      botao.setAttribute('onclick', 'window.location.reload()');
-
-      let div = document.getElementById("novo");
-      div.appendChild(mensagem);
-      div.appendChild(botao);
-  }
-
-  if (acertos === palavra.length) {
-      let mensagem = document.createElement("p");
-      let t1 = document.createTextNode("Você venceu!");
-      mensagem.appendChild(t1);
-
-      let botao = document.createElement("button");
-      let t2 = document.createTextNode("jogar novamente");
-      
-      botao.appendChild(t2);
-      botao.setAttribute('class', 'novo-bt');
-      botao.setAttribute('onclick', 'window.location.reload()');
-
-      let div = document.getElementById("novo");
-      div.appendChild(mensagem);
-      div.appendChild(botao);
+      document.getElementById("boneco").src = "assets/img/" + caminhoImagem[erros]
+      erros++
+      botao.removeAttribute('onclick');
+      if (erros == 6) {
+        perdeu = true;
+      }
+    }
   }
 }
+
+
+
+
+
+
+ //botão novo jogo e desistir eventos de click
   
-            
-           */ 
+novoJogo.addEventListener("click", function(){
       
+  window.location.reload()
+  
+})
+
+desistir.addEventListener("click", function(){
+  document.getElementById("containerForca").classList.add("mostrar")
+  document.getElementById("boneco").src = "assets/img/completo.png"
+
+})
+
+    
 
